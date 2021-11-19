ffdb6b78af6b2ff392fc83f84551eeae919fa7df
PozosG98 <pozosguzman@gmail.com>
2021-11-19 01:36:35 -0600

    Planteamiento del problema. Modelar un gerente.

--- Programa_Principal.py
+++ Programa_Principal.py
@@ -1,5 +1,13 @@
 '''
-Estos son los cambios realizados desde pycharm
+FR7A. PROGRAMACIÓN AVANZADA
+EVALUACIÓN TEMA 4. MODELAR UN GERENTE.
+
+Escriba una aplicación que modele en el paradigma orientado a objetos el siguiente escenario:
+“El gerente de la empresa es un empleado que mide 1.80 m, se llama <nombre> y tiene puesto un saco con tres botones.
+En este momento el gerente está entrevistando a una persona para su contratación.”
+Deberás generar múltiples archivos que representen la situación de la vida real en términos de programación.
+Así tendrás varias clases como son: gerente, persona, empleado, empresa, vestimenta, saco, etc.
+Debes establecer en ellas comportamientos como vestirse, trabajar y entrevistar que hagan que el escenario cobre vida.
 '''
 
 print("Archivo modificado desde pycharm")
